9a182ffab84bc5704c81b845e410d85ac65d57d3
fdech <altxfdech@gmail.com>
2022-02-03 14:30:14 +0300

    react-spa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import About from './About';
+import Users from './Users';
+import Main from './Main';
+import UserId from './UserId';
+import Error from './Error';
+
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header/>
+        {/* SPA */}
+        {/* <nav>
+          <li><Link to='/'>Main</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/users'>Users</Link></li>
+        </nav> */}
+        {/* Old vers react-router-dom (5.3.0) */}
+        {/* <Switch>
+          <Route exact path='/' component={Main}/>
+          <Route path='/about' component={About}/>
+          <Route exact path='/users' component={Users}/>
+          <Route path='/users/:userName' component={UserId}/>
+          <Route path='' component={Error}/>
+        </Switch> */}
+
+        <Routes>
+        
+          <Route exact path='/' element={<Main/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route exact path='/users' element={<Users/>}/>
+          <Route path='/users/:userName' element={<UserId/>}/>
+          <Route path='*' element={<Error/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Users.js
@@ -0,0 +1,16 @@
+import {useLocation, Link} from 'react-router-dom';
+
+function Users() {
+  let url = useLocation();
+  return (
+    <>
+        <h1>Users</h1>
+        <ul>
+          <li><Link to={`${url.pathname}/ivanov`}>Ivanov</Link></li>
+          <li><Link to={`${url.pathname}/petrov`}>Petrov</Link></li>
+        </ul>
+    </>
+  );
+}
+
+export default Users;
